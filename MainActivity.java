a300621f9818e3c5c44f27318f3222fa4f3eafe4
lucasdourado28 <lucas28061998@hotmail.com>
2017-04-18 10:32:08 -0300

    Update MainActivity.java

--- MainActivity.java
+++ MainActivity.java
@@ -16,12 +16,6 @@ import static com.example.quiz.quiz.R.string.resultMedia;
 public class MainActivity extends AppCompatActivity {
 
     //TODO declaro minhas variaveis;
-    EditText p1;
-    EditText p2;
-    EditText p3;
-    EditText p4;
-    EditText p5;
-    EditText p6;
     TextView resul;
     TextView resulMedia;
     Integer nota = 0;
@@ -50,13 +44,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //TODO pego os valores das minhas variaveis dizendo que elas viram de um editText;
-//        p1 = (EditText)findViewById(R.id.p1);
-//        p2 = (EditText)findViewById(R.id.p2);
-//        p3 = (EditText)findViewById(R.id.p3);
-//        p4 = (EditText)findViewById(R.id.p4);
-//        p5 = (EditText)findViewById(R.id.p5);
-//        p6 = (EditText)findViewById(R.id.p6);
         resul  = (TextView)findViewById(R.id.resul);
         resulMedia  = (TextView)findViewById(R.id.resulMedia);
 
@@ -81,108 +68,12 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-    //TODO método chamado quando o usuário clica no botão finalizar, chamei ele por meio de Onclick
-//    public void btnFim(View v){
-//        if (p1.getText().toString().trim().isEmpty()){ //TODO verifica se o campo está vazio
-//            p1.setError("Responda esta questão");
-//            //TODO emite mensagem de erro no campo nome quando nao for preenchido
-//        }
-//        else if (p2.getText().toString().trim().isEmpty()){ //TODO verifica se o campo está vazio
-//            p2.setError("Responda esta questão");
-//            //TODO emite mensagem de erro no campo nome quando nao for preenchido
-//        }
-//        else if (p3.getText().toString().trim().isEmpty()){ //TODO verifica se o campo está vazio
-//            p3.setError("Responda esta questão");
-//            //TODO emite mensagem de erro no campo nome quando nao for preenchido
-//        }
-//        else if (p4.getText().toString().trim().isEmpty()){ //TODO verifica se o campo está vazio
-//            p4.setError("Responda esta questão");
-//            //TODO emite mensagem de erro no campo nome quando nao for preenchido
-//        }
-//        else if (p5.getText().toString().trim().isEmpty()){ //TODO verifica se o campo está vazio
-//            p5.setError("Responda esta questão");
-//            //TODO emite mensagem de erro no campo nome quando nao for preenchido
-//        }
-//        else if (p6.getText().toString().trim().isEmpty()){ //TODO verifica se o campo está vazio
-//            p6.setError("Responda esta questão");
-//            //TODO emite mensagem de erro no campo nome quando nao for preenchido
-//        }
-//        else{
-//            //TODO verifico se a variavel é igual a resposta correta
-//            if(p1.getText().toString().equals("variavel constante")){
-//                p1.setTextColor(Color.GREEN);
-//                nota++;
-//            }else{
-//                p1.setTextColor(Color.RED);
-//            }
-//
-//            if (p2.getText().toString().equals("mesmo valor para todos metodos")){
-//                p2.setTextColor(Color.GREEN);
-//                nota++;
-//            }else{
-//                p2.setTextColor(Color.RED);
-//            }
-//
-//            if(p3.getText().toString().equals("utilizando o EXTENDS")){
-//                p3.setTextColor(Color.GREEN);
-//                nota++;
-//            }else {
-//                p3.setTextColor(Color.RED);
-//            }
-//
-//            if (p4.getText().toString().equals("permite que voce execute acoes")){
-//                p4.setTextColor(Color.GREEN);
-//                nota++;
-//            }else {
-//                p4.setTextColor(Color.RED);
-//            }
-//
-//            if(p5.getText().toString().equals("funcoes")){
-//                p5.setTextColor(Color.GREEN);
-//                nota++;
-//            }else {
-//                p5.setTextColor(Color.RED);
-//            }
-//
-//            if (p6.getText().toString().equals("logica") || p6.getText().toString().equals("Logica")){
-//                p6.setTextColor(Color.GREEN);
-//                nota++;
-//            }else {
-//                p6.setTextColor(Color.RED);
-//            }
-//
-//            Media = (nota/6)*100; //TODO calcula a média que o usuário recebeu
-//
-//            if(nota >= 4) {
-//                resul.setTextColor(Color.GREEN);
-//            }else{
-//                resul.setTextColor(Color.RED);
-//            }
-//            if(Media > 60) {
-//                resulMedia.setTextColor(Color.GREEN);
-//            }else{
-//                resulMedia.setTextColor(Color.RED);
-//            }
-//
-//        }
-//    }
-
     public void voltar(View v){
         //TODO volta para tela inicial de nome comecar
         Intent intent = new Intent(this, comecar.class);
         startActivity(intent);
     }
 
-//    public void verificaRadio(View v){
-//        switch (v.getId()){
-//            case R.id.r1:
-//                Toast.makeText(this,"O r1 está marcado "+R1.getText(), Toast.LENGTH_SHORT).show();
-//                break;
-//            case R.id.r2:
-//                Toast.makeText(this,"O r2 está marcado "+R1.getText(), Toast.LENGTH_SHORT).show();
-//                break;
-//        }
-//    }
 
     public void VerificarRadio(View v){
 
